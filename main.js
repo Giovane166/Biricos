8ca8544f0efbe28049fa7b5d7c4cde1555fe6bde
Giovane <103274655+Giovane166@users.noreply.github.com>
2024-03-27 08:14:40 -0300

    .

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ alert('Seja muito bem vindo');
 let nomeUsario = '';
 let texto = document.querySelector('span');
 
-texto.textContent = nomeUsario;
-
 while (nomeUsario == ''){
     nomeUsuario = prompt('qual o seu nome?');
 }
+
+
+texto.textContent = nomeUsario;
