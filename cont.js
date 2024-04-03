d853664fa04b08a6b3be4409146d1fd30918fea7
Giovane <103274655+Giovane166@users.noreply.github.com>
2024-04-03 08:42:07 -0300

    dfh

--- cont.js
+++ cont.js
@@ -5,4 +5,10 @@ let i = 0;
 
 for(let i = 0; i < botoes.length; i++ ){
 console.log(i);
+botoes[i].onclick('ativo'); = function(){
+    botoes[1].classList.add('ativo');
+
+} 
+
+// botoes[i].classList.remove('ativo');
 }
