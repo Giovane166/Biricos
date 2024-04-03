7269b3098ba9154a747bbab7863913c2d9038cfb
Giovane <103274655+Giovane166@users.noreply.github.com>
2024-04-03 08:46:50 -0300

    dfhdf

--- cont.js
+++ cont.js
@@ -3,10 +3,10 @@ const botoes = document.querySelectorAll('.objetivos');
 
 let i = 0;
 
-for(let i = 0; i < botoes.length; i++ ){
-console.log(i);
-botoes[i].onclick = function(){
-    
+for(let i = 0; i < botoes.length; i++ ) {
+    console.log(i);
+    botoes[i].onclick = function(){
+
     botoes[i].classList.add('ativo');
 
 } 
